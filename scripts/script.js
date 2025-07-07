d33080e8d2046fe91fb3a8d6b0ea31ceee4b2e81
Lucas Nagel <altingdesigns88@gmail.com>
2025-07-07 06:47:38 -0300

    Initial design implemented

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+    const logo = document.getElementById('logo');
+    let isFirstLogo = true;
+
+    logo.addEventListener('click', () => {
+        if (isFirstLogo) {
+            logo.src = "/images/sxtp_logo_2.png";
+        } else {
+            logo.src = "/images/sxtp_logo_1.png";
+        }
+        isFirstLogo = !isFirstLogo;
+});
+}
